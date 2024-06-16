29cd9c32456ab0f6e6b63c29e07c51d93f80c754
ccc <ccc.larc@gmail.com>
2024-06-16 21:04:18 +0800

    fix: urls

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('gpt-line-bot', include('gpt_line_bot.urls')),
+    path('gpt-line-bot/', include('gpt_line_bot.urls')),
     path('admin/', admin.site.urls),
 ]
